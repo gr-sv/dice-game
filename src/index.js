d2525b1b5be751af3e02eecf63a82d41dbc483da
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-11-23 22:24:26 +0300

    Add new styles into the project

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
