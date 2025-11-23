1a333a172549952f37995cb17ab8e63b0d170466
Sergey Gr <64542317+gr-sv@users.noreply.github.com>
2025-11-23 07:39:00 +0300

    Add reducer

---
+++ src/redux/diceSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  history: [], 
+};
+
+const diceSlice = createSlice({
+  name: 'dice', 
+  initialState,
+  reducers: {
+    rollDie: (state) => {
+      const rollValue = Math.floor(Math.random() * 7); 
+      const newRoll = {
+        value: rollValue,
+        timestamp: new Date().toLocaleTimeString('ru-RU'), 
+      };
+      state.history.push(newRoll); 
+    },
+    clearHistory: (state) => {
+      state.history = [];
+    },
+  },
+});
+
+export const { rollDie, clearHistory } = diceSlice.actions;
+export default diceSlice.reducer;
